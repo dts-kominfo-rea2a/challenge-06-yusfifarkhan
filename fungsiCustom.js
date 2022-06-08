cc36d99b140e1d9a4e92e19590eeb0a60c801b86
yusfifarkhan <93718536+yusfifarkhan@users.noreply.github.com>
2022-06-08 19:45:27 +0700

    Submit #1
    Submit 1 challenge 6 DTS REA2A

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require('fs');
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,41 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback) => {
+  const result = []; // Array of Result
+
+  // Read First JSON File
+  fs.readFile(file1, 'utf-8', (err, data) => {
+    if(err){
+      return fnCallback(err, null);
+    }
+
+    const parseData1 = JSON.parse(data);
+    result.push(parseData1.message.split(" ")[1]);
+
+    // Read Second JSON File
+    fs.readFile(file2, 'utf-8', (err, data)=>{
+      if (err) {
+        return fnCallback(err, null);
+      }
+
+      const parseData2 = JSON.parse(data);
+      result.push(parseData2[0].message.split(" ")[1]);
+
+      // Read Third JSON File
+      fs.readFile(file3, 'utf-8', (err, data)=>{
+        if(err){
+          return fnCallback(err, null);
+        }
+
+        const parseData3 = JSON.parse(data);
+        result.push(parseData3[0]['data'].message.split(" ")[1]);
+        fnCallback(null, result);
+      });
+    })
+  })
+
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
